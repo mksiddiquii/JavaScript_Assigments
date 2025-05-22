c8ddc7b063e7ba321fea63f6643ed3669e9da68d
mksiddiqui <mksiddiquii@gmail.com>
2025-05-22 15:03:39 +0500

    JavaScript assigments

---
+++ Assigments/Chapter02/app.js
@@ -0,0 +1,42 @@
+/* Task No.01 */
+var username ;
+
+/* Task No.02 */
+var myName = "Muhammad Kamran ";
+
+/* Task No.03 */
+var message = "Hello World" ;
+alert(message)
+
+/* Task No.04 */
+ 
+var firstName = "Jhone Doe " ;
+ var age = 15 ;
+ var profession = "certified Mobile Apllication Development" ;
+ alert(firstName) ;
+ alert(age) ;
+ alert(profession) ;
+
+ /* Task No.05 */
+
+ var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(pizza) ;
+
+/* Task No.06 */
+
+var email = "mksiddiquii@gmail.com";
+alert("My email address is "+email);
+
+/* Task No.07 */
+
+var book = "I am trying to learn JS From  “ A smarter way to learn JavaScript”.";
+alert(book) ;
+
+/* Task No.08 */
+var massage1 = "Yah! I can write HTML content through JavaSrcipt" ;
+document.write (massage1) ; 
+
+/* Task No.09 */
+
+var logo = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(logo)
