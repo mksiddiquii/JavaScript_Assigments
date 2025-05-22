9d06e84888ef809986d71e2b4af2453f32e172df
mksiddiqui <mksiddiquii@gmail.com>
2025-05-22 16:11:09 +0500

    update

---
+++ Assigments/Chapter04/app.js
@@ -0,0 +1,20 @@
+      /* Task No.01 */
+
+    var name1, gender, qualification ;
+    name1="Muhammad Kamran ", gender = "Male" , qualification ="Master";
+
+
+/* Task No.02 */
+    /* legal variable */
+    var user = "Muhammad Kamran";
+    var userName = "Muhammad Kamran";
+    var user_Name = "Muhammad kamran";
+    var $userName = "Muhammad Kamran";
+    var _userName= "Muhammad Kamran";
+
+    /* illegal Varibale  */
+    var 1user = "Muhammad Kamran"
+    var .user=  "Muhammad Kamran"
+    var if = "Muhammad Kamran"
+    var #user = "Muhammad Kamran"
+    var user name = "Muhammad Kamran"
