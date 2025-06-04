d7e44b8610799c7a81887b3a36b509dc00245dae
mksiddiqui <mksiddiquii@gmail.com>
2025-06-04 16:17:01 +0500

    update

---
+++ Assigments/Chapter21-25/app.js
@@ -0,0 +1,45 @@
+document.writeln(`<h1> Chapter 17 to 20 Assigments</h1>`)
+ /* Task No.01 */
+
+document.writeln(`<h3> Task No.1 </h3>`)
+
+
+/* first_name = prompt("what is you First name?");
+last_name = prompt("what is you Last name?");
+full_name = first_name +" "+ last_name ;
+document.writeln("welcome the coding world Mr."+full_name) */
+
+/* Task No.02 */
+
+document.writeln(`<h3> Task No.2 </h3>`)
+
+/* var fav_phone = prompt("What is your favorite mobile phone model:")
+document.writeln("My Favorite phone is : " + fav_phone ,"<br>")
+document.writeln("length of String is:"+fav_phone.length) */
+
+/* Task No.03 */
+
+document.writeln(`<h3> Task No.3 </h3>`)
+
+var country = "Pakistani"
+document.writeln("String: "+ country,"<br>")
+document.writeln("At 7th Index word is : "+country[7] ,"<br>")
+document.writeln("Index of n is: "+country.indexOf("n"))
+
+/* Task No.04 */
+
+document.writeln(`<h3> Task No.4 </h3>`)
+
+var greeting = "Hello World "
+document.writeln("String: "+ greeting,"<br>")
+document.writeln("Last Index of l is: "+greeting.lastIndexOf("l"))
+
+/* Task No.05 */
+
+document.writeln(`<h3> Task No.5 </h3>`)
+
+var country = "Pakistani"
+document.writeln("String: "+ country,"<br>")
+document.writeln("Character at index 3 is : "+country[3] ,"<br>")
+console.log(country);
+console.log(country);
